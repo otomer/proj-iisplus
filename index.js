011a5c3b88e0fecc269c3d9536f651d5700d9807
Oriyan <o.tomerr@gmail.com>
2017-09-13 17:36:15 +0300

    A

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var nav = [
 
 ];
 var usersRouter = require("./src/routes/userRoutes")(nav);
-app.use("/Users", usersRouter);
+//app.use("/Users", usersRouter);
 
 //Middleware - used by express first before anything else
 app.use(express.static('public'));
@@ -18,16 +18,7 @@ app.use(express.static('src/views'));
 
 app.get('/', (request, response) => {
   //response.send('Hello from Express!');
-  response.send(usersRouter);
-});
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  // res.render('error', {
-  //     message: err.message, 
-  //     error: err
-  // });
-  res.send(err.message);
+  response.send(typeof usersRouter);
 });
 
 app.listen(port, (err) => {
